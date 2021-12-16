33b7679e673e889f2a029f22d9215d977a31653e
LiaoRui999 <15695516507@163.com>
2021-12-16 12:54:32 +0800

    更新文件

--- GitHub222/src/main/java/Hello.java
+++ GitHub222/src/main/java/Hello.java
@@ -1,6 +1,8 @@
 /**
  * @author LiaoRui
  * @create 2021-12-16 10:58
+ * todo  补充你需要做的记录内容
+ * todo   需要给异常增加捕获内容
  */
 public class Hello {
     public static void main(String[] args) {
@@ -8,5 +10,6 @@ public class Hello {
         System.out.println("分支添加文件");
         System.out.println("主干在第9行添加文件");
         System.out.println("分支在第9行添加文件");
+        System.out.println("更新文件");
     }
 }
